abac77582f90a2ba98a839d37dc962aaad43465a
jeean0910 <jeean0910@gmail.com>
2025-04-03 21:17:41 -0500

    se incluyen ejercicios de operadores

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -5,12 +5,48 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 
 // 1. Crea una variable para cada operación aritmética
 
-// 2. Crea una variable para cada tipo de operación de asignación,
-//    que haga uso de las variables utilizadas para las operaciones aritméticas
+//R1
+let a = 3;
+let b = 5;
+// suma
+let suma = console.log(a + b);
+//resta
+let resta = console.log(a - b);
+//multiplicación
+let multiplicacion = console.log(a * b);
+//división
+let division = console.log(a / b);
+//modulo
+let modulo = console.log(a % b);
+//exponente
+let exponente = console.log(a ** b);
+//incremento
+let incrementoA = a++;
+console.log(a);
+//decrecimiento
+let decrecimientoA = a--;
+console.log(a);
+
+/* 2. Crea una variable para cada tipo de operación de asignación,
+   que haga uso de las variables utilizadas para las operaciones aritméticas*/
+//R2
+//suma asignada
+let a = 3;
+let b = 5;
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log(a != b || b > a);
+console.log(a <= b && b >= 5);
+console.log(a !== b);
+console.log(b >= 5);
+console.log(a <= b);
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+console.log(a == b);
+console.log(a === b);
+console.log(a >= b);
+console.log(a < 2);
+console.log(b <= a);
 
 // 5. Utiliza el operador lógico and
 
