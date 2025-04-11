71986585b56c4849c6f3e8263202d91f50ba861b
Jeean0910 <jeean0910@gmail.com>
2025-04-11 17:35:20 -0500

    se agrega soluciòn de ejercicio 4 de conditionals

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -12,19 +12,31 @@ if (names === "Jean") {
 }
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
-let user = "jean.franco.posso"
-let password = "RataBlanca_123"
+let user = "jean.franco.posso";
+let password = "RataBlanca_123";
 if (User === "jean.franco.posso" && password === "RataBlanca_123") {
-  console.log ("Ingreso exitoso")
-} else {console.log("Usuario o contraseña incorrectos")}
+  console.log("Ingreso exitoso");
+} else {
+  console.log("Usuario o contraseña incorrectos");
+}
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
-let number = -1
-if (number >= 1) {console.log("El numero es positivo")}
-else if (number === 0) {console.log ("El nùmero es cero")}
-else {console.log("el numero es negativo")}
-let jrs= "hola"
+let number = -1;
+if (number >= 1) {
+  console.log("El numero es positivo");
+} else if (number === 0) {
+  console.log("El nùmero es cero");
+} else {
+  console.log("el numero es negativo");
+}
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+let userage = 17;
+if (userage >= 18) {
+  console.log("puede votar");
+} else {
+  let yearsToVote = 18 - userage;
+  console.log(`no puede votar y le falta(n) ${yearsToVote} año(s)`);
+}
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad
