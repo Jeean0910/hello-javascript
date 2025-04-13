75b658a70936330fe7253654360aa5c54c737756
jeean0910 <jeean0910@gmail.com>
2025-04-13 13:40:40 -0500

    se modifica practica.js

--- Basic/practica.js
+++ Basic/practica.js
@@ -1,10 +1,21 @@
-let diassemana = [
-  "lunes",
-  "martes",
-  "miercoles",
-  "jueves",
-  "viernes",
-  "sabado",
-  "domingo",
-];
-console.log(diassemana);
+let edadPersonal = 11;
+let añosParaVotar = 18 - edadPersonal;
+if (edadPersonal >= 18) {
+  console.log("puede votar");
+} else if (edadPersonal < 18) {
+  console.log(`no puede votar y le quedan ${añosParaVotar} años para votar.`);
+} else {
+  console.log("Incorrecto: El valor ingresado debe ser un número");
+}
+//-----------------------------------------------------------------------------------------------
+let disponibilidad
+switch(edadPersonal)
+{case >=18: disponibilidad= 
+console.log("puede votar")
+break
+case <18:
+ disponibilidad= console.log (`no puede votar y le quedan ${añosParaVotar} años para votar.`)
+ break
+ default: disponibilidad=console.log("Incorrecto, el valor debe ser un número")   
+
+}
